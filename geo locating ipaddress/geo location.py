3a3b95008f00707bfcc2e29bdb4f3c6cf4f28774
DeadArmy <84080385+informarplanet@users.noreply.github.com>
2024-10-30 12:41:18 +0530

    Update geo location.py

--- geo locating ipaddress/geo location.py
+++ geo locating ipaddress/geo location.py
@@ -12,7 +12,7 @@ try:
     ip_address=sys.argv[1]
 except IndexError:
     ip_address=None
-access_token="fbfd711e68140f" # input your access token
+access_token="" # input your access token
 handler=ipinfo.getHandler(access_token)
 details=handler.getDetails(ip_address)
 for key, value in details .all.items():
